32648e11a68cd90b7aef396d3b733e1a864659b9
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2020-08-22 13:28:57 +0600

    dynamic burger build

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './Burger.module.css'
+import BurgerIngredient from './BurgerGredient/BurgerIngredient';
+
+const Burger = (props) => {
+    const transformedIngredient = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => { return <BurgerIngredient key={igKey + i} type={igKey} /> })
+    })
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredient}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    )
+}
+
+export default Burger
