1e7755be454aa4a5bc9157d7cb2eb4ae85f19ab8
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2020-11-07 13:06:27 +0600

    post method added in order using firebase

---
+++ src/axios-orders.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: "https://react-my-burger-b6f41.firebaseio.com/"
+})
+export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -81,7 +82,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Rafsan",
+                address: {
+                    street: "410 gawair",
+                    zipcode: "1230",
+                    country: "Bangladesh    "
+                },
+                email: "xyz@gmail.com"
+            },
+            delivaryMethod: "fastest"
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
+
+        alert("data stored. ")
     }
 
     render() {
