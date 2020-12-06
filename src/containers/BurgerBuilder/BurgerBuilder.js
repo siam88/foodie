690fe03f27b400cfb14573c75ad712a67ebea335
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2020-12-06 17:34:55 +0600

    passing ingredient via query

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -106,8 +106,17 @@ class BurgerBuilder extends Component {
         // axios.post('/orders.json', order)
         //     .then(response => this.setState({ loading: false, purchasing: false }))
         //     .catch(error => this.setState({ loading: false, purchasing: false }));
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]))
 
-        this.props.history.push('/checkout');
+        }
+        const queryStrings = queryParams.join('&');
+        console.log(queryStrings)
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryStrings
+        });
     }
 
 
